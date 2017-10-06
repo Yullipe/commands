8d10e2a64ffbc61858c02661002c6cf70d891a79
Yullipe <32579443+Yullipe@users.noreply.github.com>
2017-10-06 14:32:23 -0400

    Delete flipundefined.js

--- flipundefined.js
+++
@@ -1,8 +0,0 @@
-exports.run = (clair, message, args) => {
-	const Discord = require("Discord.js");
-	message.channel.send(message.author.username +" flipped heads!", {
-		files: [`./img/Coins/heads.jpg`
-		]
-	});
-	message.delete()
-}
