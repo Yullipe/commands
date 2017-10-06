6be33d3ea553c180a22fc9c2039afc2fbfddd57f
Yullipe <32579443+Yullipe@users.noreply.github.com>
2017-10-06 14:41:27 -0400

    Update dodge.js

--- dodge.js
+++ dodge.js
@@ -1,14 +1,4 @@
 exports.run = (clair, message, args) => {
-	if (message.author.id === "226523866825097216") return;
-	if (message.author.id === "348533726495506434") return;
-	if (message.author.id === "354919646253547523") return;
-	
-	if (message.content.length < 7) {
-		message.delete();
-		message.channel.send(":whale2:: c.dodge")
-		return;
-	}
-	
 	var textOutput = message.content.substring(7,message.content.length);
 	const Discord = require('discord.js');
 	const embed = new Discord.RichEmbed()
