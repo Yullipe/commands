c38543becfc8c498f678b169a28a5db7508406a4
Yullipe <32579443+Yullipe@users.noreply.github.com>
2017-10-06 14:38:12 -0400

    Update dodge.js

--- dodge.js
+++ dodge.js
@@ -15,6 +15,7 @@ exports.run = (clair, message, args) => {
 		.setColor(0x32CD32)
 		.addField( "Dodge: ", message.author.username, true)
 		.addField("Rolled", ":game_die: "+ Math.floor((Math.random()*7)+1) , false)
+		.addField("Note:", "if dodge fails take 2x damage", false)
 	message.channel.send({embed});
 	message.delete();
 }
