351a0cb1be09549120d0d954b9a82dbdc051afc2
Himiko <32579443+Yullipe@users.noreply.github.com>
2017-10-06 14:39:16 -0400

    Update
    Deletion of test js files

--- myjs/dice.js
+++
@@ -1,7 +0,0 @@
-exports.run = (clair, message, args) => {
-	const Discord = require("discord.js");
-	const embed = new Discord.RichEmbed()
-		.setColor(0xffffff)
-		.addField(message.author.username, ":game_die: rolled " + Math.floor((Math.random()*3)+3), false)
-	message.channel.send({embed});
-}
--- myjs/not implemented/award.js
+++
@@ -1,9 +0,0 @@
-exports.run = (clair, message, args) => {
-	const fs = require("fs");
-	let userData = JSON.parse(fs.readFileSync(`./storage/userdata.json`, "utf8"));
-	let sender = userData[message.author.id];
-	if (message.channel.id = "226523866825097216"); {
-		userData[mentions].coupons + = Math.floor((Math.random()*1)+10)
-		message.channel.send("**coupons**");
-	}
-}
