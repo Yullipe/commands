5cd5710a0f1f2ce7ef628189473aba654e4af830
Himiko <32579443+Yullipe@users.noreply.github.com>
2017-10-08 13:56:40 -0400

    Update triggered.js

--- triggered.js
+++ triggered.js
@@ -1,6 +1,6 @@
 exports.run = (clair, message, args) => {
 	const Discord = require("discord.js");
-	const photos = [`./img/trigger/1.gif`,`./img/trigger/2.gif`,`./img/trigger/3.gif`,`./img/trigger/4.gif`,`./img/trigger/5.gif`,`./img/trigger/6.gif`,`./img/trigger/7.gif`,`./img/trigger/8.gif`, ];
+	const photos = [`./img/trigger/1.gif`,`./img/trigger/2.gif`,`./img/trigger/3.gif`,`./img/trigger/4.gif`,`./img/trigger/6.gif`];
 	const rng = Math.round(Math.random()*photos.length);
 	
 	message.channel.send("**Triggered**", {
